c75d092fb47390eb08ae40dc7c3407c2fe9887e5
michelhaddad <michhadd1@gmail.com>
2022-04-25 10:20:12 +0200

    Randomly modify advice id

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let quotes = []
 const getQuotesArray = async () => {
     let response = await fetch("https://type.fit/api/quotes");
     response = await response.json();
-    return response.map(q => q.text);
+    return shuffle(response.map(q => q.text));
 }
 
 const updateQuote = async () => {
@@ -25,14 +25,36 @@ const updateQuote = async () => {
     count = (count + 1) % quotes.length;
 
     const quoteEl = document.getElementById('quote')
+    const adviceIdEl = document.getElementById('advice')
     quoteEl.classList.add('fading-text')
+    adviceIdEl.classList.add('fading-text')
 
     setTimeout(() => {
         quoteEl.innerText = text;
+        adviceIdEl.innerText = `ADVICE #${Math.ceil(Math.random() * 1000)}`
     }, 1000)
     setTimeout(() => {
         quoteEl.classList.remove('fading-text')
+        adviceIdEl.classList.remove('fading-text')
         toggleDiceRotation()
         setDiceActive(true)
     }, 2000)
+}
+
+const shuffle = (array) => {
+    let currentIndex = array.length,  randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
 }
